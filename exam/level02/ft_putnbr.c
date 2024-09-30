598b0368bfd29208457993e4a4c7fc0962d88ca8
curtyraissa <raissacurty@gmail.com>
2024-09-30 19:28:02 +0100

    fix: level02 exam subject

---
+++ exam/level02/ft_putnbr.c
@@ -0,0 +1,11 @@
+/*Assignment name  : ft_putnbr
+Expected files   : ft_putnbr.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a function that displays an integer number on the standard output.
+
+Your function must be declared as follows:
+
+void ft_putnbr(int nb);
+*/
